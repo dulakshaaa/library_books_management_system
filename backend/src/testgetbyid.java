bc80ab92677b77b4fc18697d0a84fa15bef5f3f0
dulakshaaa <dulaksharajapaksha4@gmail.com>
2026-05-30 17:35:13 +0530

    [FIX] always return null value

--- backend/src/testgetbyid.java
+++ backend/src/testgetbyid.java
@@ -3,7 +3,7 @@ public class testgetbyid {
     public static void main(String[] args){
         Book b = new Book();
         BookDAO dao = new BookDAO();
-        dao.getbyid(3);
+        b = dao.getbyid(2);
 
         System.out.println(b.getId()+" "+b.getTitle()+" "+b.getAuthor()+" "+b.getQuantity());
     }
